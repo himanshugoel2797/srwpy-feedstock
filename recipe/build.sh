4ec66f2fa851f8277fc8bad59c0dcb47f981c538
Maksim Rakitin <mrakitin@bnl.gov>
2023-06-02 13:14:28 -0400

    Add explicit conditional export of the MACOSX_DEPLOYMENT_TARGET="10.15" for OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,9 @@ cd env/python
 
 # No OMP packages for now (for consistency with PyPI), those will be available
 # once the corresponding PyPI packages are generated.
+
+if [ "$(uname -s)" == "Darwin" ]; then
+    export MACOSX_DEPLOYMENT_TARGET="10.15"
+fi
+
 ${PYTHON} -m pip install . -vv
